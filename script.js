5829b49ecfb0422d974886a358b85d69de3acaaa
Herman Siu <herman.siu@combi.local>
2026-03-30 09:51:30 +0800

    added navigation functions with arrow keys for the drop down list, added styling in css for the highlighted item in the dropdown list

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const list = document.getElementById("autocomplete-list");
 
 let selectedCity = null;
 
+let currentFocus = -1;
+
 function getWeather() {
     if (!selectedCity) {
         alert("Please select a city from the list");
@@ -14,7 +16,6 @@ function getWeather() {
         .then(res => res.json())
         .then(data => displayWeather(data));
 }
-
 
 function displayWeather(data) {
     // Check if city is invalid
@@ -36,11 +37,26 @@ function displayWeather(data) {
     document.getElementById("weatherResult").innerHTML = weatherHTML;
 }
 
+function addActive(items) {
+    if (!items) return;
+    removeActive(items);
+    if (currentFocus >= items.length) currentFocus = 0;
+    if (currentFocus < 0) currentFocus = items.length - 1;
+
+    items[currentFocus].classList.add("autocomplete-active");
+}
+
+function removeActive(items) {
+    for (let item of items) {
+        item.classList.remove("autocomplete-active");
+    }
+}
 
 input.addEventListener("input", function () {
     const value = this.value.trim();
 
     list.innerHTML = "";
+    currentFocus = -1;
     if (value.length < 2) return; // wait for 2 chars
 
     fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${value}&limit=5&appid=${apiKey}`)
@@ -73,9 +89,47 @@ input.addEventListener("input", function () {
         });
 });
 
+input.addEventListener("keydown", function (e) {
+    const items = list.getElementsByClassName("autocomplete-item");
+
+    if (e.key === "ArrowDown") {
+        currentFocus++;
+        addActive(items);
+    } 
+    else if (e.key === "ArrowUp") {
+        currentFocus--;
+        addActive(items);
+    } 
+    else if (e.key === "Enter") {
+        e.preventDefault(); // prevent form submit
+        if (currentFocus > -1 && items[currentFocus]) {
+            items[currentFocus].click();
+        }
+    }
+});
 
 document.addEventListener("click", function(e) {
     if (e.target !== input) {
         list.innerHTML = "";
     }
 });
+
+// async function getWeather() {
+//     let city = input.value.trim();
+
+//     if (!selectedCity || selectedCity.name !== city) {
+//         const geo = await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}`)
+//             .then(r => r.json());
+
+//         if (geo.length === 0) {
+//             alert("City not found");
+//             return;
+//         }
+
+//         selectedCity = geo[0];
+//     }
+
+//     fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${selectedCity.lat}&lon=${selectedCity.lon}&appid=${apiKey}&units=metric`)
+//         .then(res => res.json())
+//         .then(data => displayWeather(data));
+// }
